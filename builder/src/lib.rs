2839e8dcdd61679e2d27ba9f8489a2a9c23a783d
Moritz Willy Becher <becher@ux3d.io>
2024-06-09 01:25:15 +0200

    ex 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{self, DeriveInput};
+use proc_macro2::{Ident, Span};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast: DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
 
-    unimplemented!()
+    let gen = quote! {
+            pub struct #builder_name {
+                executable: Option<String>,
+                args: Option<Vec<String>>,
+                env: Option<Vec<String>>,
+                current_dir: Option<String>,
+            }
+
+            impl #name {
+                pub fn builder() -> #builder_name {
+                    #builder_name {
+                        executable: None,
+                        args: None,
+                        env: None,
+                        current_dir: None,
+                    }
+                }
+            }
+        };
+    gen.into()
 }
